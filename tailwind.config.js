15961bf7f4696fd3c0ccfeca17c2aa4c783a8fde
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2024-10-21 19:37:30 +0200

    add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          50: '#FEFEFE',
+          900: '#101213',
+          950: '#03090A',
+        },
+        purple: {
+          400: '#936BB6',
+          900: '#5D1CB7',
+        },
+        blue: {
+          600: '#2460BA',
+        }
+      }
+    },
   },
   plugins: [],
 }
