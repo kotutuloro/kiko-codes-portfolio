7ec463319d3311bb341be3710d83dcef16bc5c52
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2024-10-22 17:38:24 +0200

    style Projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,17 +6,19 @@ function Projects() {
     title: `Project Title ${i + 1}`,
     date: "Sept 2024",
     summary: "some quick project description super fast here just a couple lines maybe even like three lines max here",
-    image: "https://placehold.co/500x400"
+    image: "https://placehold.co/480x300"
   }))
   const projectElems = projectInfo.map(info => <ProjectCard {...info} />)
 
   return (
     <div>
-      <h1>project_highlights</h1>
-      <div>
+      <h1 className="font-mono text-center text-3xl text-purple-400">project_highlights</h1>
+      <div className="my-5 grid grid-cols-2 gap-5">
         {projectElems}
       </div>
-      <span><a href="#">more projects...</a></span>
+      <div className="font-mono text-right text-xl">
+        <a href="#" className="underline">more projects...</a>
+      </div>
     </div>
   )
 }
