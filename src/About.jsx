ea76fee0f0707a2306f8c482ff2dc3473cd2140f
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2024-10-22 12:50:23 +0200

    style About

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,10 @@
 function About() {
   return (
-    <div>
-      <img src="https://placehold.co/300x300" alt="Kiko's headshot" />
-      <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Egestas feugiat porta nascetur vulputate hendrerit pharetra consectetur molestie. Felis phasellus consequat lorem maecenas at ligula. Praesent aliquam est turpis, velit morbi blandit habitant. Imperdiet mauris amet lobortis ultrices curabitur dui ultrices ad.</p>
+    <div className="max-w-2xl mx-auto flex gap-6 items-center text-center">
+      <img src="https://placehold.co/300x300" alt="Kiko's headshot" className="rounded-full size-52" />
+      <p className="w-full">
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Egestas feugiat porta nascetur vulputate hendrerit pharetra consectetur molestie. Felis phasellus consequat lorem maecenas at ligula. Praesent aliquam est turpis, velit morbi blandit habitant. Imperdiet mauris amet lobortis ultrices curabitur dui ultrices ad.
+      </p>
     </div>
   )
 }
