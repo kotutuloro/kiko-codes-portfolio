2361b37dbfb72dce6b227f57b1e38442c7ecd4ab
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2024-10-25 13:52:14 +0200

    add about photo

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,12 @@
+import myPhoto from "./assets/picrew2.png"
+
 function About() {
   return (
-    <div className="max-w-2xl mx-auto flex flex-wrap gap-6 items-center justify-center text-center">
-      <img src="https://placehold.co/300x300" alt="Kiko's headshot" className="rounded-full size-52" />
+    <div className="max-w-2xl mx-auto flex flex-wrap-reverse gap-6 items-center justify-center text-center">
       <p className="basis-7/12 grow shrink-0">
         Lorem ipsum odor amet, consectetuer adipiscing elit. Egestas feugiat porta nascetur vulputate hendrerit pharetra consectetur molestie. Felis phasellus consequat lorem maecenas at ligula. Praesent aliquam est turpis, velit morbi blandit habitant. Imperdiet mauris amet lobortis ultrices curabitur dui ultrices ad.
       </p>
+      <img src={myPhoto} alt="Kiko's headshot" className="rounded-full size-52" />
     </div>
   )
 }
