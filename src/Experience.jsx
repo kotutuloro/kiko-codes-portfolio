a2f355938bdb7dc98e95fd7f15a5027321fb995d
Kikelomo Otutuloro <k.otutuloro@gmail.com>
2024-11-13 19:32:23 +0100

    styling and layout fixes for Experience section

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,10 +1,12 @@
+// import resume from "/docs/Kiko Otutuloro Resume Fall 2024.pdf";
+
 function Experience() {
   return (
     <div className="text-center">
       <h1 className="font-mono text-3xl text-purple-400">exp&shy;erience</h1>
       <div className="divide-dotted *:my-4 md:flex md:divide-x-2 *:md:px-4 *:lg:px-8">
         <div>
-          <h2 className="font-mono text-xl italic underline decoration-purple-400 decoration-dotted">
+          <h2 className="font-mono text-2xl italic underline decoration-purple-400 decoration-dotted">
             skills
           </h2>
           <ul className="leading-snug">
@@ -20,10 +22,10 @@ function Experience() {
         </div>
 
         <div className="shrink-0 grow basis-2/3">
-          <h2 className="font-mono text-xl italic underline decoration-purple-400 decoration-dotted">
+          <h2 className="font-mono text-2xl italic underline decoration-purple-400 decoration-dotted">
             employment
           </h2>
-          <ul className="leading-snug *:flex *:items-center *:justify-between *:gap-2 *:py-1 first:*:*:text-left last:*:*:text-right last:*:*:italic">
+          <ul className="grid gap-2 leading-snug *:col-span-2 *:grid *:grid-cols-subgrid *:items-center first:*:*:text-left last:*:*:text-right last:*:*:italic">
             <li>
               <span>Senior Software Engineer @ Flexport</span>
               <span>(Sep 2023 - Mar 2024)</span>
